2b40ad908403480903cdc21aaf7d5c29da600bae
Barsbek Makhmatov <makhmatovb@gmail.com>
2023-07-08 13:23:55 +0600

    update input

--- src/components/UI/input/Input.jsx
+++ src/components/UI/input/Input.jsx
@@ -1,24 +1,37 @@
-import { OutlinedInput, styled } from '@mui/material'
+import { TextField, styled } from '@mui/material'
 import React, { forwardRef } from 'react'
 
-export const Input = forwardRef(({ placeholder, width, height, type }, ref) => {
-   return (
-      <div>
-         <StyledInput
-            ref={ref}
-            placeholder={placeholder || 'введите что-нибудь'}
-            width={width}
-            height={height}
-            type={type || 'text'}
-         />
-      </div>
-   )
-})
+export const Input = forwardRef(
+   ({ type, placeholder, helperText, ...props }, ref) => {
+      return (
+         <div>
+            <StyledInput
+               type={type}
+               placeholder={placeholder || 'введите что-нибудь'}
+               ref={ref}
+               {...props}
+               error
+               helperText={helperText || 'Incorrect entry.'}
+            />
+         </div>
+      )
+   }
+)
 
-const StyledInput = styled(OutlinedInput)((props) => ({
-   width: props.width || '40%',
-   height: props.height || '10%',
-   '&.Mui-focused > .MuiOutlinedInput-notchedOutline': {
+const StyledInput = styled(TextField)((props) => ({
+   width: props.width || '100%',
+   height: props.height || '37px',
+   padding: props.padding || '10px, 8px, 10px, 16px',
+
+   '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+         borderColor: 'gray',
+      },
+      '&:hover fieldset': {
+         border: '2px solid gray',
+      },
+   },
+   '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
       borderColor: 'gray',
    },
 }))
