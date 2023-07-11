dc8b705406558cca10e71a04b1af9ddcd652a1a4
ITAziret26 <azirettoichubekov263@gmail.com>
2023-07-11 13:24:05 +0600

    create Date-Picker

---
+++ src/components/UI/date-picker/Date-Picker.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import { useState } from 'react'
+import dayjs from 'dayjs'
+import 'dayjs/locale/ru'
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { styled } from '@mui/material'
+
+dayjs.locale('Ru')
+
+export default function DatePickerValue() {
+   const [value, setValue] = useState(dayjs('2022-04-10'))
+
+   return (
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+         <DemoContainer components={['DatePicker']}>
+            <StyledDate
+               value={value}
+               onChange={(newValue) => setValue(newValue)}
+            />
+         </DemoContainer>
+      </LocalizationProvider>
+   )
+}
+
+const StyledDate = styled(DatePicker)(() => ({
+   '& .MuiInputBase-input': {
+      padding: '10px 16px',
+      display: 'inline-flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+   },
+}))
