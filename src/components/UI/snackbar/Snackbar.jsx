768d1a94d1aae0ae14905e0fc0764a492f3f52d1
Пользователь <kursanovNizambek.27@gmail.com>
2023-07-12 14:48:14 +0600

    finished snackbar

--- src/components/UI/snackbar/Snackbar.jsx
+++ src/components/UI/snackbar/Snackbar.jsx
@@ -1,42 +1,92 @@
 import React from 'react'
 import { styled } from '@mui/material'
-import { toast, ToastContainer } from 'react-toastify'
+import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-export function ShowSnackbar({ message, description, severity }) {
-   toast[severity](
-      <StyledCustomToast>
-         <h3>{message}</h3>
-         <p>{description}</p>
-      </StyledCustomToast>
-   )
-   return <StyledToastContainer autoClose={false} severity={severity} />
+export const toastSnackbar = () => {
+   const toastType = (severity, message, description) =>
+      toast[severity](
+         <>
+            <Message>{message}</Message>
+            <Description>{description}</Description>
+         </>,
+         {
+            position: 'top-right',
+            autoClose: false,
+            closeOnClick: true,
+            theme: 'colored',
+         }
+      )
+   return { toastType }
 }
 
-const StyledToastContainer = styled(ToastContainer)(({ severity }) => ({
-   paddingRight: '31.25rem',
+export function Snackbar() {
+   return <StyledToastContainer />
+}
+
+const StyledToastContainer = styled(ToastContainer)(() => ({
+   textAlign: 'start',
+   paddingRight: '30.25rem',
+
+   '& .Toastify__toast--success': {
+      background: '#eafbe7',
+
+      '& .Toastify__close-button': {
+         path: {
+            fill: '#828282',
+         },
+      },
+
+      '& .Toastify__toast-icon': {
+         marginTop: '1rem',
+         height: '8vh',
+         svg: {
+            display: 'none',
+         },
+      },
+   },
+   '& .Toastify__toast--error': {
+      background: '#fff1f0',
+
+      '& .Toastify__close-button': {
+         path: {
+            fill: '#828282',
+         },
+      },
+
+      '& .Toastify__toast-icon': {
+         marginTop: '1rem',
+         height: '8vh',
+         svg: {
+            display: 'none',
+         },
+      },
+   },
+
    '& .Toastify__toast': {
-      backgroundColor: severity === 'success' ? '#F0FFF1' : '#FFF1F0',
       width: '32vw',
-      height: '6.25rem',
+      height: '6.25',
+      path: {
+         background: '#eafbe7',
+      },
    },
 }))
 
-const StyledCustomToast = styled('div')(() => ({
-   h3: {
-      color: '#000',
-      fontFamily: 'Inter',
-      fontSize: '1rem',
-      fontStyle: 'normal',
-      fontWeight: '500',
-      lineHeight: 'normal',
-   },
-   p: {
-      color: ' var(--tertiary-dark-gray, #646464)',
-      fontFamily: 'Inter',
-      fontSize: '0.875rem',
-      fontStyle: 'normal',
-      fontWeight: '400',
-      lineHeight: 'normal',
-   },
+const Description = styled('h4')(() => ({
+   color: 'var(--tertiary-dark-gray, #646464)',
+   fontFamily: 'Inter',
+   fontSize: ' 0.875rem',
+   fontStyle: 'normal',
+   fontWeight: '400',
+   lineHeight: 'normal',
+   marginTop: '10px',
+}))
+
+const Message = styled('p')(() => ({
+   color: '#000',
+   fontFamily: 'Inter',
+   fontSize: ' 1rem',
+   fontStyle: 'normal',
+   fontWeight: '500',
+   lineHeight: 'normal',
 }))
