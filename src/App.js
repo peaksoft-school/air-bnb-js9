72dc4afef0fe481b053061909df41ab9e7d5bcba
Barsbek Makhmatov <makhmatovb@gmail.com>
2023-07-13 19:52:39 +0600

    delete app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
-import { RatingStars } from './components/UI/rating/RatingStars'
-
 function App() {
-   return (
-      <div>
-         AirBnB
-         <RatingStars />
-      </div>
-   )
+   return <div>AirBnB</div>
 }
 export default App
