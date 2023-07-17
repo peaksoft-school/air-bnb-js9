b8dae04cde125dbf015f6d4de76d5c5d4a3b5051
Пользователь <kursanovNizambek.27@gmail.com>
2023-07-17 14:25:58 +0600

    fexed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom'
-=======
 import { ThemeProvider } from '@mui/material'
->>>>>>> 48e9edbd708a40a72540938e75a3ebf9aebbdc87
 import './index.css'
 import App from './App'
 import { theme } from './assets/styles/theme'
@@ -13,14 +9,8 @@ import { theme } from './assets/styles/theme'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
    <React.StrictMode>
-<<<<<<< HEAD
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>
-=======
       <ThemeProvider theme={theme}>
          <App />
       </ThemeProvider>
->>>>>>> 48e9edbd708a40a72540938e75a3ebf9aebbdc87
    </React.StrictMode>
 )
