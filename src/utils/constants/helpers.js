536712bb336797ea7feeb9b41a24871006d97b10
Пользователь <kursanovNizambek.27@gmail.com>
2023-07-19 19:29:06 +0600

    delete folders

--- src/utils/constants/helpers.js
+++ src/utils/constants/helpers.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 import detailHouse1 from '../../assets/images/houseDeteil1.png'
 import detailHouse2 from '../../assets/images/houseDeteil2.png'
 import detailHouse3 from '../../assets/images/houseDeteil3.png'
 import detailHouse4 from '../../assets/images/houseDeteil4.png'
-=======
-import house1 from '../../assets/images/popularHouse1.png'
-import house2 from '../../assets/images/popularHouse2.png'
-import house3 from '../../assets/images/popularHouse3.png'
->>>>>>> 7e34678ef8e715d8158ecf875f4e9e981c346c3a
 
 export const regions = [
    { id: 'option1', name: 'Batken' },
@@ -32,7 +26,6 @@ export const price = [
    { id: 'option13', name: 'High to low' },
 ]
 
-<<<<<<< HEAD
 export const house = [
    {
       id: 1,
@@ -42,28 +35,5 @@ export const house = [
       imgHouse4: detailHouse4,
       imgHouse5: detailHouse3,
       imgHouse6: detailHouse4,
-=======
-export const popularHouse = [
-   {
-      img: `${house1}`,
-      title: 'Asman guest house',
-      address: '723510 Osh Muzurbek Alimbekov 9/7',
-      price: '$26',
-      rating: '3.4',
-   },
-   {
-      img: `${house2}`,
-      title: 'Asman guest house',
-      address: '723510 Osh Muzurbek Alimbekov 9/7',
-      price: '$26',
-      rating: '3.4',
-   },
-   {
-      img: `${house3}`,
-      title: 'Asman guest house',
-      address: '723510 Osh Muzurbek Alimbekov 9/7',
-      price: '$26',
-      rating: '3.4',
->>>>>>> 7e34678ef8e715d8158ecf875f4e9e981c346c3a
    },
 ]
