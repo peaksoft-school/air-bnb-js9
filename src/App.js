da23d7d654e5a259af567ab31800615a9c5dc624
Gulzat Sydykova <sydykova561@gmail.com>
2023-07-20 15:48:36 +0600

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Cards } from './components/UI/cards/Cards'
+import { data } from './utils/constants/helpers'
 
 function App() {
-   return <div>AirBnB</div>
+   return (
+      <div>
+         AirBnB
+         <Cards data={data} />
+      </div>
+   )
 }
 export default App
