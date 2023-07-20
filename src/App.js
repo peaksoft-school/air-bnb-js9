9addb39963dcf29a30f364eb12a69b0804aa7c84
Gulzat Sydykova <sydykova561@gmail.com>
2023-07-20 15:50:40 +0600

    cleaned App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from 'react'
-import { Cards } from './components/UI/cards/Cards'
-import { data } from './utils/constants/helpers'
 
 function App() {
-   return (
-      <div>
-         AirBnB
-         <Cards data={data} />
-      </div>
-   )
+   return <div>AirBnB</div>
 }
 export default App
