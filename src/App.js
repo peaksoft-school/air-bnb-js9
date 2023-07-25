26b85e442d3b0c48ff2bf3fa81faac324f7507fb
Gulzat Sydykova <sydykova561@gmail.com>
2023-07-25 12:04:25 +0600

    cleaned app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import React from 'react'
-import { Booked } from './components/UI/booked/Booked'
 
 function App() {
-   const data = [
-      {
-         price: 233,
-         name: 'gulzat',
-         email: '@gmail.com',
-         checkin: '20.10.2023',
-         checkout: '20.10.2023',
-      },
-   ]
-   return (
-      <div>
-         {data?.map((item) => {
-            return <Booked item={item} />
-         })}
-      </div>
-   )
+   return <div>AirBnB</div>
 }
 export default App
