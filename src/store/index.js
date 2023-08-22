27ab18a32374397b014c3b101ca935f270c38367
Пользователь <kursanovNizambek.27@gmail.com>
2023-08-22 14:35:50 +0600

    save

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { applicationSlice } from './admin-application/ApplicationSlice'
-import { anouncementSlice } from './upload/anouncementSlice'
-import { authSlice } from './auth/authSlice'
 
 export const store = configureStore({
    reducer: {
       [applicationSlice.name]: applicationSlice.reducer,
-      [anouncementSlice.name]: anouncementSlice.reducer,
-      [authSlice.name]: authSlice.reducer,
    },
 })
