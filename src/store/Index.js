cedfdf8f90a71690cb9aa480d6096266d5bb0dca
Пользователь <kursanovNizambek.27@gmail.com>
2023-08-23 17:06:15 +0600

    change file code index.js

--- src/store/Index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { applicationSlice } from './admin-application/ApplicationSlice'
-
-export const store = configureStore({
-   reducer: {
-      [applicationSlice.name]: applicationSlice.reducer,
-   },
-})
