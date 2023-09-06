a0e61ac2ccf51948a535a68f2a86bf5d89e02eb6
Gulzat Sydykova <sydykova561@gmail.com>
2023-09-06 12:30:37 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { AppRoutes } from './routes/AppRoutes'
-import { Favorite } from './components/favorite/Favorite'
 
 function App() {
    return (
       <div>
          <AppRoutes />
-         <Favorite />
       </div>
    )
 }
